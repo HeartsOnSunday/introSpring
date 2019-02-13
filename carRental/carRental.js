ab2da4b0dd9464a1749d3791df616e72d5e1ad20
Heartsonsunday <leahgdg@hotmail.com>
2019-02-13 00:07:17 -0500

    itemized car report

--- carRental/carRental.js
+++ carRental/carRental.js
@@ -2,7 +2,7 @@
 
 var totallySketchCarRentalSpot = "JavaScript Hotel";
 
-var carTypes = ["Smashed Batmobile", "Dodge Dart", "Clown Car", "Van full of \'Candy\'", "Ford Pinto"];
+var carTypes = ["Smashed Batmobile", "Dodge Dart", "Clown Car", "Van full of 'Candy'", "Ford Pinto"];
 var carPrices = [ 175, 185, 140, 215, 230];
 var availableCarsByRegistration = [["RI-101", "RI-102", "MA-103", "CT-104"], ["RI-105", "AZ-106", "TX-107", "RI-108"], ["FL-201", "CT-202", "MA-203", "FL-204"],["LA-301", "VT-302", "RI-303"], ["MA-304", "VT-305"]];
 var reservedCars = [[], [], [], [], []];
@@ -47,15 +47,15 @@ function reservationReport() {
        totalReservationProfit += reservedCars[i].length * carPrices[i];
        }
        if (reservedCars[0]) {
-        console.log(reservedCars.length, "happy customers drove off in a ", carTypes[0]);
+        console.log(reservedCars[0].length, "happy customers drove off in a ", carTypes[0]);
     }  if (reservedCars[1]) {
-        console.log(reservedCars.length, "happy customers drove off in a ", carTypes[1]);
+        console.log(reservedCars[1].length, "happy customers drove off in a ", carTypes[1]);
     } if (reservedCars[2]) {
-        console.log(reservedCars.length, "happy customers drove off in a ", carTypes[2]);
+        console.log(reservedCars[2].length, "happy customers drove off in a ", carTypes[2]);
     } if (reservedCars[3]) {
-        console.log(reservedCars.length, "happy customers drove off in a ", carTypes[3]);
+        console.log(reservedCars[3].length, "happy customers drove off in a ", carTypes[3]);
     } if (reservedCars[4]) {
-        console.log(reservedCars.length, "happy customers drove off in a ", carTypes[4]);
+        console.log(reservedCars[4].length, "happy customers drove off in a ", carTypes[4]);
     } 
     return totalReservationProfit;
 };
