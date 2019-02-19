8d72417832bd26871347f73c332fd848ff0ff8c2
Heartsonsunday <leahgdg@hotmail.com>
2019-02-18 23:48:40 -0500

    modfied

--- jsHOTEL/hotel.js
+++ jsHOTEL/hotel.js
@@ -3,11 +3,9 @@
 var hotelName = "JavaScript Hotel";
 
 var roomTypes = ["Twin", "Single", "Double", "Queen", "King"];
-var roomPrices = [ 75, 85, 100, 150, 200 ];
-
+var roomPrices = [ 75, 85, 100, 150, 200];
 var availableRoomNumbers = [[101, 102, 103, 104], [105, 106, 107, 108], [201, 202, 203, 204],[301, 302, 303], [304, 305]];
 var bookedRooms = [[], [], [], [], []];
-var roomPrices = [ 75, 85, 100, 150, 200];
 
 function availableRoomQty() {
     var totalAvailableRoomQty = 0;
@@ -22,10 +20,6 @@ function availableRoomQty() {
 function totalAvailableRoomsReport() {
     console.log("Total Available Room Count:", availableRoomQty());
 }
-
-
-// var availableRoomNumbers = [[101, 102, 103, 104], [105, 106, 107, 108], [201, 202, 203, 204],[301, 302, 303], [304, 305]];
-// var bookedRooms = [[], [], [], [], []];
 
 function bookTwin() {
     bookedRooms[0].push(availableRoomNumbers[0].pop());
