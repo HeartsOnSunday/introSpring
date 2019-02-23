3009b7f5606b44a1c4ed89face2d248b86872c74
Heartsonsunday <leahgdg@hotmail.com>
2019-02-23 11:14:32 -0500

    Current Salaries by Id works

--- sararyReport/deptSalary/deptSalary.js
+++ sararyReport/deptSalary/deptSalary.js
@@ -12,14 +12,14 @@ var employeeId = [];
 var employeeName = [];
 var salaries = [];
 
-/* Process 'load_dept_names.txt' file */
+// Process 'load_dept_names.txt' file  populte deptartmentId and departments
 fs.readFile('load_dept_names.txt', 'utf8', function(err, data) {
     if (err) throw err;
     
     var deptDataClean = data.replace(/INSERT INTO `departments` VALUES \n/g, "");
     var deptDataArray = deptDataClean.split('\n');
 
-    for (var i = 0; i < deptDataArray.length; i++){
+    for (var i = 0; i < deptDataArray.length; i++) {
         //populate single-dimensional arrays with DATA
         departmentId.push(deptDataArray[i].slice(2,6));
         departments.push(deptDataArray[i].slice(9,-3));
@@ -29,13 +29,12 @@ fs.readFile('load_dept_names.txt', 'utf8', function(err, data) {
         employeeName.push([]);
         salaries.push([]);
     }
-    // console.log(departmentId);
-    // console.log(departments);
+
+    console.log(`Departments are: ${departmentId}`);
+    console.log(`Departments are: ${departments}`);
     // console.log(employeeId);
     // console.log(employeeName);
     // console.log(salaries);
-
-
 });
 
 /* Process 'load_dept_emp.txt' file */
@@ -55,11 +54,56 @@ fs.readFile('load_dept_emp.txt', 'utf8', function(err, data) {
         //console.log(departmentId.indexOf(employeeDataArray[i].slice(8,12)));
         //console.log(employeeId[departmentId.indexOf(employeeDataArray[i].slice(7,11))].push(employeeDataArray[i].slice(2,6));)
     }
-
-
-    
-    console.log(employeeId);
+    console.log(`Employee ids are: ${employeeId}`);
    // console.log(employeeDataClean);
    // console.log(employeeDataArray);
 });
 
+
+//procure the employee current salary by Id
+fs.readFile('load_salaries1.txt', 'utf8', function(err, data) {
+if (err) throw err;
+var salaryAgregate = data.split('\n');
+//console.log(`Salaries are: ${salaryAgregate}`);
+
+//current salary for Employee Id 
+for (var j = 0; j < salaryAgregate.length; j++) {
+    if (salaryAgregate[j].slice(27,31) == 9999) {
+    console.log(`Employee ${salaryAgregate[j].slice(1,6)} earned $: ${salaryAgregate[j].slice(7,12)} in year: ${salaryAgregate[j].slice(27,31)}`);
+}
+}
+});
+
+
+
+console.log(`Employee names are: ${employeeName}`);
+
+
+
+
+
+
+
+
+
+/*
+
+
+// Process 'load_employee.txt' file 
+fs.readFile('load_employee.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+
+    var employeeNameArray = data.slice("\n");
+   // console.log(employeeNameArray);
+
+ 
+for (var i = 0; i < employeeNameArray.length; i++) {
+    employeeName.push(employeeNameArray[i].slice(22,-20));
+}
+console.log(employeeName);
+//splice data from employeeNameArray (22,-19)
+//employeeName[].push(emplyeeNameArray.slice(22,-19));
+//}
+});
+
+*/
