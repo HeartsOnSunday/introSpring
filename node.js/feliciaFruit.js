9e4aa79f7ff13d967470677c9de69c1bb1b0756d
Heartsonsunday <leahgdg@hotmail.com>
2019-03-15 17:04:51 -0400

    new calculation

---
+++ node.js/feliciaFruit.js
@@ -0,0 +1,8 @@
+var fruitPrices = [1.00, 1.25, 1.50, 1.75];
+var fruitSales = [50, 25, 15, 10];
+var fruitNames = ["Apples", "Bananas", "Mangoes", "Honeydew"];
+
+for (var i = 0; i < fruitNames.length; i++) {
+	console.log("Total", fruitNames[i], "sales are", fruitSales[i] * fruitPrices[i])};
+
+
